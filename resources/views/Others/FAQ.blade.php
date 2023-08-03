f5fde89239b1e8ae778d4deb5ccd62c051250baf
002Be <Dakdra@outlook.com>
2023-08-03 19:39:11 +0300

    SSS sayfasının içeriği eklendi

--- resources/views/Others/FAQ.blade.php
+++ resources/views/Others/FAQ.blade.php
@@ -4,10 +4,60 @@
 
 <div class="container">
     <div class="mb-5">
-        <h1 class="text-center"><b>Gizlilik Politikası</b></h1>
+        <h1 class="text-center"><b>Sıkça Sorulan Sorular</b></h1>
     </div>
     <div>
-        
+        <section>
+            <!-- <h3 class="text-center mb-4 pb-2 text-primary fw-bold">SSS</h3> -->
+            <p class="text-center mb-5">
+                Aşağıda en sık sorulan soruların yanıtlarını bulun
+            </p>
+
+            <div class="row">
+                <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-primary"><i class="far fa-paper-plane text-primary pe-2"></i>Basit bir soru?</h6>
+                <p>
+                    <!-- <strong><u>Kesinlikle!</u></strong> -->
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique voluptate dolore nisi porro cum asperiores corrupti quisquam dolorem? Perferendis ut facilis nihil alias a, architecto aperiam! Dolorem autem delectus accusantium?
+                </p>
+                </div>
+
+                <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-primary"><i class="fas fa-pen-alt text-primary pe-2"></i>Basit bir soru?</h6>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus eveniet odit alias quasi quos vitae? Exercitationem, sequi ullam libero, id rerum minus repudiandae ab vero, ipsa distinctio vel perferendis incidunt?
+                </p>
+                </div>
+
+                <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-primary"><i class="fas fa-user text-primary pe-2"></i>Basit bir soru?</h6>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dolorum ea saepe earum, magni recusandae debitis nisi iure omnis ipsum necessitatibus veritatis voluptas dolor cum sunt nam rerum quibusdam fuga?
+                </p>
+                </div>
+
+                <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-primary"><i class="fas fa-rocket text-primary pe-2"></i>Basit bir soru?</h6>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae facere debitis eaque quod omnis distinctio ratione. Mollitia minus neque obcaecati vitae fugiat sit, beatae illo cum, ea quod suscipit rem.
+                </p>
+                </div>
+
+                <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-primary"><i class="fas fa-home text-primary pe-2"></i>Basit bir soru?</h6>
+                <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima atque labore porro commodi quasi illo quis quas asperiores repudiandae quia. Exercitationem voluptatibus earum corrupti nisi velit voluptates magni harum voluptatem?
+                </p>
+                </div>
+
+                <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-primary"><i class="fas fa-book-open text-primary pe-2"></i>Basit bir soru?</h6>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad nobis ab provident harum obcaecati sequi, officiis impedit labore velit dolorem quis explicabo totam, enim ratione nulla ea fugit, culpa officia!
+                </p>
+                </div>
+            </div>
+        </section>
     </div>
 </div>
 
